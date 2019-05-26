af88ccb5ebb84939a5cae9eda2ff085434dd319b
yoavrotems <yoavrotems97@gmail.com>
2019-05-26 15:10:46 +0300

    Update 1.1.25.sh

--- cfg/1.1.0/1.1.25.sh
+++ cfg/1.1.0/1.1.25.sh
@@ -1,1 +1,1 @@
-df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null | head -n 100
