27e347f3bbf8b37939952035e253ab99090a5b9f
yoavrotems <yoavrotems97@gmail.com>
2020-11-22 14:01:20 +0200

    Add support for scripts in definitions
    Now we can run scripts in audit field, so all of the old files are no longer needed.

--- cfg/1.1.0/6.2.15.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-for i in $(cut -s -d: -f4 /etc/passwd | sort -u ); do 
-	grep -q -P "^.*?:[^:]*:$i:" /etc/group 
-	if [ $? -ne 0 ]; then 
-		echo "Group $i is referenced by /etc/passwd but does not exist in /etc/group" 
-	fi 
-done
